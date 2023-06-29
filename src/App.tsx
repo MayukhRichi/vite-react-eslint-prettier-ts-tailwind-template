46edff54afb59d86f59817c0499fa3ec2dc5ec98
MayukhRichi <mayukh007raychaudhuri@gmail.com>
2023-06-29 10:57:11 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-blue-100">
+      <h1 className="text-5xl font-bold text-blue-600">
+        vite-react-eslint-prettier-ts-tailwind-template
+      </h1>
+    </div>
+  );
+}
+
+export default App;
