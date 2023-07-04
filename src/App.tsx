bfc34639adabee0e383ca678e43a76eea2b79aab
MayukhRichi <mayukh007raychaudhuri@gmail.com>
2023-07-04 09:55:26 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="min-h-screen flex justify-center items-center bg-blue-100">
+    <div className="flex min-h-screen items-center justify-center bg-blue-100">
       <h1 className="text-5xl font-bold text-blue-600">
         vite-react-eslint-prettier-ts-tailwind-template
       </h1>
