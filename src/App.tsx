c7effbc3b0d2a497a1aefa9bbbb05833efa2f38d
MayukhRichi <mayukh007raychaudhuri@gmail.com>
2023-07-05 18:29:13 +0530

    seventh commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,44 @@
-function App() {
+import { ErrorBoundary } from "react-error-boundary";
+
+function Header() {
   return (
-    <>
-      <h1 className="m-4 text-2xl font-bold text-sky-500">
-        vite-react-eslint-prettier-ts-tailwind-template
-      </h1>
-      <button
-        type="button"
-        className="primary-button m-4"
-        onClick={() => console.count("clicked")}
+    <h1 className="m-8 text-3xl font-bold text-sky-500">
+      vite-react-eslint-prettier-ts-tailwind-template
+    </h1>
+  );
+}
+
+function FaultyComponent() {
+  throw new Error("Error 503: Service Unavailable");
+  return <h2>Hello React</h2>;
+}
+
+function App() {
+  const FallbackComponent = ({ error }: { error: Error }) => {
+    return (
+      <>
+        <pre style={{ color: "red" }}>{error.message}</pre>
+        <a href="/">
+          <button type="button" className="danger-button m-4">
+            Try Again
+          </button>
+        </a>
+      </>
+    );
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <Header />
+      <ErrorBoundary
+        FallbackComponent={FallbackComponent}
+        onError={(error: Error, info: { componentStack: string }) => {
+          console.log(error.message, info.componentStack);
+        }}
       >
-        Click
-      </button>
-    </>
+        <FaultyComponent />
+      </ErrorBoundary>
+    </div>
   );
 }
 
