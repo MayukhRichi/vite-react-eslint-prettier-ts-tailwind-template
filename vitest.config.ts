9f15063969a408aa3b2b55eff991310551741556
MayukhRichi <mayukh007raychaudhuri@gmail.com>
2023-07-10 18:37:49 +0530

    twelfth commit

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: "happy-dom",
+    globals: true,
+    environment: "jsdom",
     coverage: {
       provider: "v8",
       reporter: ["text", "html"],
